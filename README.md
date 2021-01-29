8db0fce63ad05f0ef8b0d4f6f9de2d2a575bb83f
suu3 <71166763+suu3@users.noreply.github.com>
2021-01-29 23:53:34 +0900

    until #2.20

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Wetube
 
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Profile
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- app.js
+++ app.js
@@ -4,23 +4,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use(localsMiddleware);
 
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
-
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 export default app;
