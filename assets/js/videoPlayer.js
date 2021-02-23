209fb89a66763e661c7e018023b38eefbada751c
suu3 <71166763+suu3@users.noreply.github.com>
2021-02-24 00:18:45 +0900

    ~#8.4

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,19 +1,129 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsVolume");
 
+//재생바
 function handlePlayClick() {
-  if (videoPlayer.paused) {
-    videoPlayer.play();
+    if (videoPlayer.paused) {
+        videoPlayer.play();
+        playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    } else {
+        videoPlayer.pause();
+        playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+}
+
+//화면크기
+function exitFullScreen() {
+    fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+    fullScrnBtn.addEventListener("click", goFullScreen);
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+function goFullScreen() {
+    if (videoContainer.requestFullscreen) {
+        videoContainer.requestFullscreen();
+    } else if (videoContainer.mozRequestFullScreen) {
+        videoContainer.mozRequestFullScreen();
+    } else if (videoContainer.webkitRequestFullscreen) {
+        videoContainer.webkitRequestFullscreen();
+    } else if (videoContainer.msRequestFullscreen) {
+        videoContainer.msRequestFullscreen();
+    }
+    fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+    fullScrnBtn.removeEventListener("click", goFullScreen);
+    fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
+//시간 형식
+const formatDate = seconds => {
+    const secondsNumber = parseInt(seconds, 10);
+    let hours = Math.floor(secondsNumber / 3600);
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+    let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (totalSeconds < 10) {
+        totalSeconds = `0${totalSeconds}`;
+    }
+    return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+//현재 재생 시간
+function getCurrentTime() {
+    currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
+}
+
+//비디오 길이
+function setTotalTime() {
+    const totalTimeString = formatDate(videoPlayer.duration);
+    totalTime.innerHTML = totalTimeString;
+    setInterval(getCurrentTime, 1000);
+}
+
+//비디오 재생 완료
+function handleEnded() {
+  videoPlayer.currentTime = 0;
+  playBtn.innerHTML = '<i class="fas fa-play"></i>';
+}
+
+//볼륨
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+      videoPlayer.muted = false;
+      volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+      volumeRange.value = videoPlayer.volume;
   } else {
-    videoPlayer.pause();
+      volumeRange.value = 0;
+      videoPlayer.muted = true;
+      volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 
+// 볼륨 조절
+function handleDrag(event) {
+  const {
+    target: { value }
+  } = event;
+  videoPlayer.volume = value;
+  if (value >= 0.6) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else if (value >= 0.2) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+  } else {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+  }
+}
+
+//시작
 function init() {
-  playBtn.addEventListener("click", handlePlayClick);
+    videoPlayer.volume = 0.5;
+    playBtn.addEventListener("click", handlePlayClick);
+    volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScrnBtn.addEventListener("click", goFullScreen);
+    videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+    videoPlayer.addEventListener("ended", handleEnded);
+    volumeRange.addEventListener("input", handleDrag);
 }
 
 if (videoContainer) {
-  init();
+    init();
 }
