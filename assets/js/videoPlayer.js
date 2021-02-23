2122fd4ffe21f95a049767b4a117ff4d8ee49325
suu3 <71166763+suu3@users.noreply.github.com>
2021-02-24 05:39:38 +0900

    #10.1

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -7,6 +7,13 @@ const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("jsVolume");
 
+const registerView = () => {
+    const videoId = window.location.href.split("/videos/")[1];
+    fetch(`/api/${videoId}/view`, {
+      method: "POST"
+    });
+  };
+
 //재생바
 function handlePlayClick() {
     if (videoPlayer.paused) {
@@ -81,6 +88,7 @@ function setTotalTime() {
 
 //비디오 재생 완료
 function handleEnded() {
+  registerView();
   videoPlayer.currentTime = 0;
   playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
